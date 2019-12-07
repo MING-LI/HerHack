7d7a37f174a9041e160dc3135ccf2578b919af31
JohnC <yhjchanaa@connect.ust.hk>
2019-12-08 03:16:10 +0800

    add firestoreservice

--- HerHack/AppDelegate.swift
+++ HerHack/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        FirebaseApp.configure()
         GMSServices.provideAPIKey(Constants.Key)
         GMSPlacesClient.provideAPIKey(Constants.Key)
       
---
+++ HerHack/FirestoreService.swift
@@ -0,0 +1,41 @@
+//
+//  FirestoreService.swift
+//  HerHack
+//
+//  Created by JohnC on 8/12/2019.
+//  Copyright © 2019 pdni. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FirestoreService {
+    private var db: Firestore
+    init () {
+        FirebaseApp.configure()
+        self.db = Firestore.firestore()
+    }
+
+    func retrieveData (from collection:String, document: String? = nil) {
+        if let doc = document {
+            db.collection(collection).document(doc).getDocument { (document, error) in
+                if let document = document, document.exists {
+                    let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                    print("Document data: \(dataDescription)")
+                } else {
+                    print("Document does not exist")
+                }
+            }
+        } else {
+            db.collection(collection).getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("\(document.documentID) => \(document.data())")
+                    }
+                }
+            }
+        }
+    }
+}
