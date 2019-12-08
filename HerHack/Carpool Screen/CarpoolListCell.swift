c298ade3f9d3f423ed6d84944854ff45105cfdbd
LabLamb <lablambworks@gmail.com>
2019-12-08 13:19:48 +0800

    Added AM PM

--- HerHack/Carpool Screen/CarpoolListCell.swift
+++ HerHack/Carpool Screen/CarpoolListCell.swift
@@ -212,8 +212,8 @@ class CarpoolListCell: UITableViewCell {
     
     func plugData(data: Carpool) {
         self.ownerLabel.text = data.user_offer_ride
-        self.startTimeLabel.text = data.start_at.toString(format: "HH:mm")
-        self.endTimeLabel.text = data.end_at.toString(format: "HH:mm")
+        self.startTimeLabel.text = data.start_at.toString(format: "HH:mm a")
+        self.endTimeLabel.text = data.end_at.toString(format: "HH:mm a")
         self.sourceLocLabel.text = data.source
         self.destLocLabel.text = data.destination
         self.carpoolCountLabel.text = "\(data.users_request_ride.count)/\(data.offered_seats)"
