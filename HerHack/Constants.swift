96f1f68d6c6a0b90531bf714dae48c8d267b032d
LabLamb <lablambworks@gmail.com>
2019-12-08 13:33:28 +0800

    Remove unused code

--- HerHack/Constants.swift
+++ HerHack/Constants.swift
@@ -24,26 +24,11 @@ struct Constants {
         SmallFont: UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
     )
     
-////    static let boldFont = UIFont(name: "GillSans-SemiBold", size: 22)
-//    static let BoldFont = UIFont.boldSystemFont(ofSize: 18)
-//
-////    static let semiBold = UIFont(name: "GillSans-SemiBold", size: 16)
-//    static let SemiBold = UIFont.boldSystemFont(ofSize: 16)
-//
-////    static let regularFont = UIFont(name: "GillSans", size: 20)
-//    static let RegularFont = UIFont.systemFont(ofSize: UIFont.systemFontSize)
-//
-////    static let smallFont = UIFont(name: "GillSans-Light", size: 16)
-//    static let SmallFont = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
-    
     static let Colors = (
         GreyColor: UIColor(red:0.70, green:0.70, blue:0.70, alpha:1.0),
         LightGreyColor: UIColor(red:0.84, green:0.84, blue:0.84, alpha:1.0),
         Green: UIColor(red:0.00, green:0.65, blue:0.33, alpha:1.0),
         Red: UIColor(red: 219/255.0, green: 0.0/255.0, blue: 17/255.0, alpha: 1)
     )
-    
-//    static let GreyColor = UIColor(red: 250.0/255.0, green: 250.0/255.0, blue: 250.0/255.0, alpha:1)
-//    static let lightGreyColor =
 }
 
