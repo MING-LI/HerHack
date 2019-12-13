6969480575d0e978e4853c9f24323cf2c6b0241e
JohnC <yhjchanaa@connect.ust.hk>
2019-12-13 12:49:37 +0800

    add poll form

---
+++ HerHack/Model/Data Model/CommentForm.swift
@@ -0,0 +1,15 @@
+//
+//  CommentForm.swift
+//  HerHack
+//
+//  Created by JohnC on 13/12/2019.
+//  Copyright © 2019 pdni. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct CommentFormData {
+    var rating: Int = 1
+    var comment: String = ""
+}
